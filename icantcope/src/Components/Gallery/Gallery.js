3e48f161b66ed869b0a058bc9872822ae091ea37
João Araújo <juauzaraujo@gmail.com>
2023-03-29 23:10:15 +0100

    new favicons, removed mock colors

--- icantcope/src/Components/Gallery/Gallery.js
+++ icantcope/src/Components/Gallery/Gallery.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import '../Gallery/Gallery.css';
 import Item from '../Item/Item.js';
+import Placeholder from '../../Images/Placeholder.png'
 
 class Gallery extends React.Component {
     render() {
         return <div className='Gallery'>
-            <Item url="https://pyxis.nymag.com/v1/imgs/1d1/117/9af62e05e25ed279f53830b7805ef667ad-The-Weeknd.rsquare.w700.jpg" alt="photo of the weeknd" />
+            <Item url={Placeholder} alt="photo of the weeknd" />
+            <Item url={Placeholder} alt="photo of the weeknd" />
+            <Item url={Placeholder} alt="photo of the weeknd" />
+            <Item url={Placeholder} alt="photo of the weeknd" />
+            <Item url={Placeholder} alt="photo of the weeknd" />
+            <Item url={Placeholder} alt="photo of the weeknd" />
+            <Item url={Placeholder} alt="photo of the weeknd" />
             <Item url="https://pyxis.nymag.com/v1/imgs/1d1/117/9af62e05e25ed279f53830b7805ef667ad-The-Weeknd.rsquare.w700.jpg" alt="photo of the weeknd" />
             <Item url="https://pyxis.nymag.com/v1/imgs/1d1/117/9af62e05e25ed279f53830b7805ef667ad-The-Weeknd.rsquare.w700.jpg" alt="photo of the weeknd" />
             <Item url="https://pyxis.nymag.com/v1/imgs/1d1/117/9af62e05e25ed279f53830b7805ef667ad-The-Weeknd.rsquare.w700.jpg" alt="photo of the weeknd"/>
--- icantcope/src/index.js
+++ icantcope/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './Fonts/MeltedPotato-Regular.otf';
+import './Fonts/Pixelcastle-Regular.otf';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
