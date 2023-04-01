4a1c6a2ef785b425c1610126bbcf938cfff66ce9
João Araújo <juauzaraujo@gmail.com>
2023-04-01 11:11:50 +0100

    god

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../Footer/Footer.css';
+
+class Footer extends React.Component {
+    render() {
+        
+        return <div className='Footer'>
+            <a href="https://twitter.com/gvns0x" rel="noreferrer" target="_blank">gvns</a>
+            <iframe src="https://icantcope.substack.com/embed" width="480" height="100" frameborder="0" ></iframe>
+        </div>
+    }
+}
+
+export default Footer;
