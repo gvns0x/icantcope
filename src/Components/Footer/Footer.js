1e7f34a0d41fb2367a13f3df701a07b1462b9fbd
João Araújo <juauzaraujo@gmail.com>
2023-04-01 12:48:09 +0100

    add title to iframe

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -24,7 +24,7 @@ class Footer extends React.Component {
         return (
             <div className='Footer'>
                 <div className='FooterSubscribe'>
-                    <iframe src="https://icantcope.substack.com/embed" width="480" height="100" frameborder="0"></iframe>
+                    <iframe src="https://icantcope.substack.com/embed" title="Newsletter subscription form" width="480" height="100" frameborder="0"></iframe>
                     {/* <div className='OverlayButton'>
                     <p className='OverlayButtonCopy'>Send it</p>
                 </div> */}
