b6f1923f4ab9ba9e4415cbaa8557da502d80795f
João Araújo <juauzaraujo@gmail.com>
2023-04-01 15:29:02 +0100

    fix horizontal scroll on mobile

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -23,8 +23,9 @@ class Footer extends React.Component {
 
         return (
             <div className='Footer'>
+                <div className='FooterContent'>
                 <div className='FooterSubscribe'>
-                    <iframe src="https://icantcope.substack.com/embed" title="Newsletter subscription form" width="480" height="100" frameborder="0"></iframe>
+                    <iframe src="https://icantcope.substack.com/embed" title="Newsletter subscription form" max-width="480" height="100" frameborder="0"></iframe>
                     {/* <div className='OverlayButton'>
                     <p className='OverlayButtonCopy'>Send it</p>
                 </div> */}
@@ -32,7 +33,8 @@ class Footer extends React.Component {
                 </div>
                 <a href="https://twitter.com/gvns0x" rel="noreferrer" target="_blank" onMouseOver={this.handleLinkHover} onMouseOut={this.handleLinkMouseOut}>gvns↗</a>
                 <p className={`Warning ${isHovered ? 'visible' : ''}`}>This will take you to my profile on Twitter. Be intentional. Don't get lost.</p>
-            </div>
+                </div>
+                </div>
         );
     }
 }
