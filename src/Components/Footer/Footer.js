d9d38d2e008af232c90eabb99b3e6152e28e7547
João Araújo <juauzaraujo@gmail.com>
2023-04-01 12:32:41 +0100

    iterate on footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,19 +2,38 @@ import React from 'react';
 import '../Footer/Footer.css';
 
 class Footer extends React.Component {
+    state = {
+        isHovered: false
+    }
+
+    handleLinkHover = () => {
+        this.setState({
+            isHovered: true
+        });
+    }
+
+    handleLinkMouseOut = () => {
+        this.setState({
+            isHovered: false
+        });
+    }
+
     render() {
-        
-        return <div className='Footer'>
-            <div className='FooterSubscribe'>
-                <iframe src="https://icantcope.substack.com/embed" width="480" height="100" frameborder="0" ></iframe>
-                {/* <div className='OverlayButton'>
-                <p className='OverlayButtonCopy'>Send it</p>
-            </div> */}
-                <p className='FooterSubscribeDescription'>Reflections in your inbox. No spam – that would be <i>against</i> the point.</p>
+        const { isHovered } = this.state;
+
+        return (
+            <div className='Footer'>
+                <div className='FooterSubscribe'>
+                    <iframe src="https://icantcope.substack.com/embed" width="480" height="100" frameborder="0"></iframe>
+                    {/* <div className='OverlayButton'>
+                    <p className='OverlayButtonCopy'>Send it</p>
+                </div> */}
+                    <p className='FooterSubscribeDescription'>Reflections in your inbox. No spam – that would defeat the purpose.</p>
+                </div>
+                <a href="https://twitter.com/gvns0x" rel="noreferrer" target="_blank" onMouseOver={this.handleLinkHover} onMouseOut={this.handleLinkMouseOut}>gvns↗</a>
+                <p className={`Warning ${isHovered ? 'visible' : ''}`}>This will take you to my profile on Twitter. Be intentional. Don't get lost.</p>
             </div>
-            <a href="https://twitter.com/gvns0x" rel="noreferrer" target="_blank">gvns↗</a>
-            
-        </div>
+        );
     }
 }
 
