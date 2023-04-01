fbb25ac2b62ab185cbbc39ae7cb43e43f3c14fa9
João Araújo <juauzaraujo@gmail.com>
2023-04-01 11:37:52 +0100

    add subs footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,8 +5,15 @@ class Footer extends React.Component {
     render() {
         
         return <div className='Footer'>
-            <a href="https://twitter.com/gvns0x" rel="noreferrer" target="_blank">gvns</a>
-            <iframe src="https://icantcope.substack.com/embed" width="480" height="100" frameborder="0" ></iframe>
+            <div className='FooterSubscribe'>
+                <iframe src="https://icantcope.substack.com/embed" width="480" height="100" frameborder="0" ></iframe>
+                {/* <div className='OverlayButton'>
+                <p className='OverlayButtonCopy'>Send it</p>
+            </div> */}
+                <p className='FooterSubscribeDescription'>Reflections in your inbox. No spam – that would be <i>against</i> the point.</p>
+            </div>
+            <a href="https://twitter.com/gvns0x" rel="noreferrer" target="_blank">gvns↗</a>
+            
         </div>
     }
 }
